a028c1c7955df0bf257da3ee9bb9cff65173e173
raptorvka <61007366+raptorvka@users.noreply.github.com>
2022-05-27 18:02:11 +0100

    Update CONSTS.ts

--- CometChatWorkspace/src/CONSTS.ts
+++ CometChatWorkspace/src/CONSTS.ts
@@ -1,5 +1,5 @@
 export const COMETCHAT_CONSTANTS = {
-  APP_ID: "APP_ID",
-  REGION: "REGION",
-  AUTH_KEY: "AUTH_KEY",
+  APP_ID: "210827b0849ff5ba",
+  REGION: "eu",
+  AUTH_KEY: "df1cddbe86b3a92a71031c4bcc566779f3656e5a",
 };
